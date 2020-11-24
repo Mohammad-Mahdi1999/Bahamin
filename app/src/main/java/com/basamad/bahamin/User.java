84c7b4162121217925699e1f96b08a09b59dd4ea
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-24 14:07:51 +0330

    User Classs

---
+++ app/src/main/java/com/basamad/bahamin/User.java
@@ -0,0 +1,20 @@
+package com.basamad.bahamin;
+
+public class User {
+    String userID;
+    String userName;
+    String userFirstName;
+    String userLastName;
+    String userProfileImage;
+    String userGender;
+    String userAge;
+    String userPassword;
+    String userLocation;
+    String userPhoneNumber;
+    String userHomeAddress;
+    String userPayment;
+    String userfavorite;
+    String userSearches;
+    String userEmail;
+    
+}
