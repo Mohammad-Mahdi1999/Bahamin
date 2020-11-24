576aea7c6af3c45d5d69980dcf20d2f3bbe60f62
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-24 14:17:54 +0330

    User has-a UserVerify Class

---
+++ app/src/main/java/com/basamad/bahamin/UserVerify.java
@@ -0,0 +1,10 @@
+package com.basamad.bahamin;
+
+public class UserVerify {
+    String userName;
+    String userID;
+    String userPassword;
+    String userEmail;
+    String userPhoneNumber;
+
+}
