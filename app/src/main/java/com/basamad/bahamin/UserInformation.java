48ba71775bb1441d4966bfc4c237d7af5f7fce32
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-24 14:18:44 +0330

    User has-a UserInformation Class

---
+++ app/src/main/java/com/basamad/bahamin/UserInformation.java
@@ -0,0 +1,10 @@
+package com.basamad.bahamin;
+
+public class UserInformation {
+
+    String userFirstName;
+    String userLastName;
+    String userProfileImage;
+    String userGender;
+    String userAge;
+}
