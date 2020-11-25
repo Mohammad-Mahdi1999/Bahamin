25d86f58abdaa185fd481c84c8880a303284aea9
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-25 12:11:15 +0330

    Contructor generated - User Class

--- app/src/main/java/com/basamad/bahamin/User.java
+++ app/src/main/java/com/basamad/bahamin/User.java
@@ -1,12 +1,23 @@
 package com.basamad.bahamin;
 
+import android.provider.Contacts;
+
 public class User {
 
-    String userLocation;
-    String userHomeAddress;
-    String userPayment;
-    String userfavorite;
-    String userSearches;
+    public User(String userLocation, String userHomeAddress, String userPayment, String userfavorite, String userSearches) {
+        this.userLocation = userLocation;
+        this.userHomeAddress = userHomeAddress;
+        this.userPayment = userPayment;
+        this.userfavorite = userfavorite;
+        this.userSearches = userSearches;
+    }
+
+    public String userLocation;
+    public String userHomeAddress;
+    public String userPayment;
+    public String userfavorite;
+    public String userSearches;
+
 
 
 }
