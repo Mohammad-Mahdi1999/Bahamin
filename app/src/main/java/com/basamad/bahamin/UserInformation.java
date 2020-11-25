191123db550ea824fc7e9691f361c000d55044ab
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-25 12:12:19 +0330

    Contructor generated - UserInformation Class

--- app/src/main/java/com/basamad/bahamin/UserInformation.java
+++ app/src/main/java/com/basamad/bahamin/UserInformation.java
@@ -1,10 +1,22 @@
 package com.basamad.bahamin;
 
 public class UserInformation {
+    public UserInformation(String userFirstName, String userLastName, String userProfileImage, String userGender, String userAge)
+    {
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userProfileImage = userProfileImage;
+        this.userGender = userGender;
+        this.userAge = userAge;
+    }
 
-    String userFirstName;
-    String userLastName;
-    String userProfileImage;
-    String userGender;
-    String userAge;
+    //Variables
+    public String userFirstName;
+    public String userLastName;
+    public String userProfileImage;
+    public String userGender;
+    public String userAge;
+
+
+
 }
