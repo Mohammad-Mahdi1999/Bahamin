248c036c4d96ca377ff30816fedaf6ebe4319244
Mahdi <mahdi.farahbakhsh99@gmail.com>
2020-11-26 13:25:11 +0330

    Register Class Added

---
+++ app/src/main/java/com/basamad/bahamin/Register.java
@@ -0,0 +1,45 @@
+package com.basamad.bahamin;
+
+public class Register {
+
+    //Variables
+    public String newuserName;
+    public String newuserID;
+    public String newuserPassword;
+    public String newuserPhoneNumber;
+
+    //Constructor
+
+
+    public String getNewuserName() {
+        return newuserName;
+    }
+
+    public void setNewuserName(String newuserName) {
+        this.newuserName = newuserName;
+    }
+
+    public String getNewuserID() {
+        return newuserID;
+    }
+
+    public void setNewuserID(String newuserID) {
+        this.newuserID = newuserID;
+    }
+
+    public String getNewuserPassword() {
+        return newuserPassword;
+    }
+
+    public void setNewuserPassword(String newuserPassword) {
+        this.newuserPassword = newuserPassword;
+    }
+
+    public String getNewuserPhoneNumber() {
+        return newuserPhoneNumber;
+    }
+
+    public void setNewuserPhoneNumber(String newuserPhoneNumber) {
+        this.newuserPhoneNumber = newuserPhoneNumber;
+    }
+}
